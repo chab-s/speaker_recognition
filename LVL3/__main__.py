bcdea2040f6ed2024066c1ca53d6c45283690049
Matteo Chabbert <chabb.mat@gmail.com>
2025-02-25 03:20:44 +0100

    upgrades: GMM_UBM corrections and optimal n_component implementations.

--- LVL3/__main__.py
+++ LVL3/__main__.py
@@ -1,6 +1,3 @@
-from src import SpeakerClassifier
-from src import SpeakerClustering
-
 from src import SpeakerFeatureExtractor
 from src import ConfigManager
 from src import GMM_UBM
@@ -11,86 +8,6 @@ import numpy as np
 import os
 import matplotlib.pyplot as plt
 conf = ConfigManager("conf.json")
-
-def train_model(save: bool = True):
-    dataset_path = conf.get('database.database_path')
-    speakers = conf.get('database.speakers')
-    inference_data_path = conf.get('model.inference_data_path')
-
-    model = conf.get('model.model_type')
-    test_size = conf.get('model.train_size')
-    random_state = conf.get('model.random_state')
-    save_path = conf.get('model.save_path')
-
-    dataset_speakers = SpeakerFeatureExtractor(dataset_path, speakers)
-    dataset_speakers.features_extraction()
-
-    X = dataset_speakers.df.filter(like='mfcc').values
-    y = dataset_speakers.df['speaker']
-
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=random_state)
-
-    X_train, X_inference, y_train, y_inference = train_test_split(X_train, y_train, test_size=0.1, random_state=random_state)
-
-    np.save(os.path.join(inference_data_path, 'inference_data.npy'), X_inference)
-    np.save(os.path.join(inference_data_path, 'inference_labels.npy'), y_inference)
-
-    svm = SpeakerClassifier(model=model)
-    svm.train(X_train, y_train)
-    accuracy = svm.evaluate(X_test, y_test)
-    print(f"Accuracy: {accuracy}")
-
-    if save:
-        svm.save_model(save_path)
-
-def infer_model(weigths_path):
-    dataset_path = conf.get('database.database_path')
-    speakers = conf.get('database.speakers')
-    inference_data_path = conf.get('model.inference_data_path')
-    model = conf.get('model.model')
-
-    dataset_speakers = SpeakerFeatureExtractor(dataset_path, speakers)
-    dataset_speakers.features_extraction()
-
-    X = np.load(os.path.join(inference_data_path, 'inference_data.npy'), allow_pickle=True)
-    y_true = np.load(os.path.join(inference_data_path, 'inference_labels.npy'), allow_pickle=True)
-
-    svm = SpeakerClassifier(model=model)
-    svm.load_model(weigths_path)
-
-    y_pred = svm.model.predict(X)
-    accuracy = np.mean(y_pred == y_true)
-
-    print(f"Prediction: {y_pred[:5]}, True label: {y_true[:5]}")
-    print(f"Inference Accuracy: {accuracy:.4f}")
-
-def use_clustering():
-    dataset_path = conf.get('database.database_path')
-    speakers = conf.get('database.speakers')
-    inference_data_path = conf.get('model.inference_data_path')
-
-    model = conf.get('model.model')
-    test_size = conf.get('model.train_size')
-    random_state = conf.get('model.random_state')
-    save_path = conf.get('model.save_path')
-
-    dataset_speakers = SpeakerFeatureExtractor(dataset_path, speakers)
-    dataset_speakers.features_extraction()
-
-    X = dataset_speakers.df.filter(like='mfcc').values
-    y = dataset_speakers.df['speaker']
-
-    clustering = SpeakerClustering(model=model, n_clusters=dataset_speakers.df["speaker"].nunique())
-    clustering.test_scaler(np.stack(X))
-
-    dataset_speakers.df['cluster'] = clustering.clusterization(X)
-
-    evaluation = clustering.evaluate_clusters(dataset_speakers.df['speaker'], dataset_speakers.df['cluster'])
-
-    # visualiser = Visualiser(dataset_speakers.df)
-    # visualiser.mfcc_histogram()
-    # visualiser.mfcc_boxplot()
-    # visualiser.normality_test(test='anderson')
 
 def ubm(save: bool = True, test: bool = False):
     dataset_path = conf.get('database.database_path')
@@ -171,6 +88,7 @@ def ubm(save: bool = True, test: bool = False):
 
     print(f"Accuracy: {score/len(y_inference)}")
 
+
 def ubm_test():
     speakers = conf.get('database.speakers')
     n_components = conf.get('gmm_ubm.n_components')
@@ -192,19 +110,12 @@ def ubm_test():
             score+=1
     print(f'Accuracy: {score/len(y)}')
 
-
-
-
-
 if __name__ == '__main__':
-
-    # train_model()
-    # weights_path = conf.get("model.save_path")
-    # infer_model(weights_path)
-
-    # use_clustering()
     ubm(save=True)
     ubm_test()
+
+
+
 
 
 
---
+++ LVL3/src/DatasetManager.py
@@ -0,0 +1,104 @@
+import os
+from fileinput import filename
+
+import numpy as np
+import pandas as pd
+import librosa
+import sidekit
+import json
+import multiprocessing
+import logging
+from .logger_config import get_logger
+from .config_manager import ConfigManager
+conf = ConfigManager("conf.json")
+
+
+class AudioDatasetLoader:
+
+    def __init__(self, dataset_path: str):
+        self.dataset_path = dataset_path
+        self.df = self._load_audio_files()
+
+    def _load_audio_files(self):
+        data = []
+        for dir in os.listdir(self.dataset_path):
+            audio_path = os.path.join(self.dataset_path, dir)
+            if os.path.isdir(audio_path):
+                audio_files = [f for f in os.listdir(audio_path) if f.endswith(('.wav', '.mp3'))]
+                for audio in audio_files:
+                    file_path = os.path.join(audio_path, audio)
+                    y, sr = librosa.load(file_path, sr=None)
+                    duration = librosa.get_duration(y=y, sr=sr)
+
+                    data.append(
+                        {
+                            "AudioCorpus": dir,
+                            "filename": audio,
+                            "path": audio_path,
+                            "signal": y,
+                            "sampling_rate": sr,
+                            "duration": duration
+                         }
+                    )
+
+        return pd.DataFrame(data)
+
+
+class SpeakerFeatureExtractor(AudioDatasetLoader):
+    def __init__(self, dataset_path, speakers):
+        super().__init__(dataset_path)
+        self.df = self.df[self.df['AudioCorpus'].isin(speakers)].copy()
+        self.df = self.df.rename(columns={'AudioCorpus': 'speaker'})
+        self.logger = get_logger(self.__class__.__name__)
+
+    def adapte_duration(self, target_duration: int):
+        data, new_signals, new_durations, files, paths = [], [], [], [], []
+        for speaker, group in self.df.groupby('speaker'):
+            sampling_rate = self.df['sampling_rate'].iloc[0]
+            target_sample = int(target_duration * sampling_rate)
+            temp_signal = np.array([])
+
+            for _, row in group.iterrows():
+                temp_signal = np.concatenate([temp_signal, row['signal']])
+                files.append(self.df['filename'])
+                paths.append(self.df['path'])
+                if len(temp_signal) >= target_sample:
+                    new_signals = temp_signal[:target_sample]
+                    new_durations = len(new_signals) / sampling_rate
+                    data.append(
+                        {
+                            "speaker": speaker,
+                            "filename": files,
+                            "path": paths,
+                            "signal": new_signals,
+                            "sampling_rate": target_sample,
+                            "duration": new_durations
+                        }
+                    )
+
+                    temp_signal = new_signals[target_sample:]
+
+        return pd.DataFrame(data)
+
+    def features_extraction(self):
+        def mfcc_mean_std(y, sr):
+            mfccs = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=conf.get('database.n_mfcc'))
+            mfccs = np.hstack((np.mean(mfccs, axis=1), np.std(mfccs, axis=1)))
+            return mfccs
+
+        mfcc_features = np.vstack(self.df.apply(lambda row: mfcc_mean_std(row['signal'], row['sampling_rate']), axis=1))
+
+        num_coeffs = mfcc_features.shape[1]
+        mfcc_columns = [f'mfcc_{i}' for i in range(num_coeffs)]
+
+        mfcc_df = pd.DataFrame(mfcc_features, columns=mfcc_columns)
+        self.df = pd.concat([self.df.reset_index(drop=True), mfcc_df], axis=1)
+
+class NoiseFeatureExtractor(AudioDatasetLoader):
+    """Classe pour extraire les caractéristiques des bruits audio."""
+
+    def __init__(self, dataset_path, noises):
+        super().__init__(dataset_path)
+        self.df = self.df[self.df['AudioCorpus'].isin(noises)].copy()
+
+
